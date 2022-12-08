23405a375dda4c8e7b148a56fbdf9e9df4c4195e
Albertus A <angkuwjr@yahoo.com>
2022-12-09 05:42:51 +0700

    Membuat Paralllax Effect dan Menambah Cinemachine Camera

--- Assets/Scripts/Behavior/Background.cs
+++ Assets/Scripts/Behavior/Background.cs
@@ -5,38 +5,35 @@ using UnityEngine;
 public class Background : MonoBehaviour
 {
     public Vector2 parallaxEffectMultiplier;
-
-    private Vector3 lastPosition;
-
-
     public GameObject target;
-    private float textureUnitSizeX;
+
+    private Vector2 lastPosition;
+
+    private Vector2 sizeSprite;
+    
     // Start is called before the first frame update
     void Start()
     {
         lastPosition = target.transform.position;
-        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-        Texture2D texture = sprite.texture;
-        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        sizeSprite = GetComponent<SpriteRenderer>().bounds.size;
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
         if(target == null){
             return;
         }
-        Vector3 deltaMovement = target.transform.position - lastPosition;
-        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x,
-                                            deltaMovement.y * parallaxEffectMultiplier.y);
-        lastPosition = target.transform.position;
-        if(Mathf.Abs(target.transform.position.x - transform.position.x) >= textureUnitSizeX){
-            float offsetPositionX = (target.transform.position.x - transform.position.x) % textureUnitSizeX;
-            transform.position = new Vector3(target.transform.position.x+ offsetPositionX, target.transform.position.y);
+        float temp = (target.transform.position.x * (1 - parallaxEffectMultiplier.x));
+        float dist = (target.transform.position.x * parallaxEffectMultiplier.x);
+
+        transform.position = new Vector3(lastPosition.x + dist, 
+                                         transform.position.y, 
+                                         transform.position.z);
+        if(temp > lastPosition.x + sizeSprite.x){
+            lastPosition += sizeSprite;
+        }else if(temp < lastPosition.x - sizeSprite.x){
+            lastPosition -= sizeSprite;
         }
-
-
-        //transform.position = target.transform.position;
-        
     }
 }
